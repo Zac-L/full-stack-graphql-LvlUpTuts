7a0084732c1d00e6f76ede6c8d848c39d3feaaf4
Zac-L <Zachlichens@gmail.com>
2018-04-12 23:42:32 -0700

    quick line-through styling dependent on wether or not a goal has been checked

--- apollo/imports/ui/resolutions/Goal.js
+++ apollo/imports/ui/resolutions/Goal.js
@@ -14,11 +14,16 @@ class Goal extends Component {
   };
 
   render() {
+    const { goal } = this.props;
     return(
 
       <li>
-        <input type="checkbox" onChange={this.toggleGoal} checked={this.props.goal.completed} />
-        {this.props.goal.name}
+        <input type="checkbox" onChange={this.toggleGoal} checked={goal.completed} />
+        <span style={{
+          textDecoration: goal.completed ? 'line-through' : 'none'
+        }}>
+          {goal.name}
+        </span>
       </li>
 
     )

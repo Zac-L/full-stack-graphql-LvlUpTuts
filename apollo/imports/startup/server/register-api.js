c2503a98f136c46fc700e50603572a047e17ab69
Zac-L <Zachlichens@gmail.com>
2018-04-12 20:46:20 -0700

    wierd update bug work around for meteor

--- apollo/imports/startup/server/register-api.js
+++ apollo/imports/startup/server/register-api.js
@@ -10,7 +10,7 @@ import GoalsSchema from '../../api/goals/Goal.graphql';
 import GoalsResolvers from '../../api/goals/resolvers';
 
 
-// this comment is to get around a wierd update bug
+// this comment is to get around a wierd update
 const typeDefs = [
   ResolutionsSchema,
   UsersSchema,
